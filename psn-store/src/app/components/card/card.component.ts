d3a9b7e55493763707fc6156003d845613d8e2ee
Joana de Angeles <Joana de Angeles>
2023-10-08 16:01:29 -0300

    implementação do dinamismo

--- psn-store/src/app/components/card/card.component.ts
+++ psn-store/src/app/components/card/card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+	@Input()
+	gameCover: string = ''
+	@Input()
+	gameLabel: string = ''
+	@Input()
+	gameType: string = ''
+	@Input()
+	gamePrice: string = ''
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
